06a57281c2dea58f93aa22da5665b1f5a812a4a2
Sonya test <sonya-vasilyeva@yandex.ru>
2023-03-24 12:31:48 +0300

    third commit

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/BonusServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+// import ru.netology.javaqa.javaqamvn.services.BonusService;
+
+public class BonusServiceTest {
+    @Test
+    public void testRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        int actual = service.calcBonus (5000, true);
+        //System.out.println("1. " + expected + " == ? == " + actual);
+
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testUnregisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        int expected = 50;
+        int actual = service.calcBonus (5000, false);
+       // System.out.println("2. " + expected + " == ? == " + actual);
+
+        Assertions.assertEquals(expected, actual) ;
+    }
+}
